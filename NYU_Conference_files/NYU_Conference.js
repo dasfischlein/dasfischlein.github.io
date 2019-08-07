44371d38f39cc490d379343744c086d477f9557b
Shieva Kleinschmidt <dasfischlein@gmail.com>
2019-08-07 09:03:20 -0700

    Copying old website

---
+++ NYU_Conference_files/NYU_Conference.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20090411
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,135),url:'NYU_Conference_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'NYU_Conference_files/stroke_1.png'},{rect:new IWRect(2,-2,155,4),url:'NYU_Conference_files/stroke_2.png'},{rect:new IWRect(157,-2,4,4),url:'NYU_Conference_files/stroke_3.png'},{rect:new IWRect(157,2,4,135),url:'NYU_Conference_files/stroke_4.png'},{rect:new IWRect(157,137,4,5),url:'NYU_Conference_files/stroke_5.png'},{rect:new IWRect(2,137,155,5),url:'NYU_Conference_files/stroke_6.png'},{rect:new IWRect(-2,137,4,5),url:'NYU_Conference_files/stroke_7.png'}],new IWSize(159,139))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('NYU_Conference_files/NYU_ConferenceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ NYU_Travel_files/NYU_Travel.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20090411
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('NYU_Travel_files/NYU_TravelMoz.css')
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('NYU_Travel_files','shapeimage_2','0');IMpreload('NYU_Travel_files','shapeimage_2','1');IMpreload('NYU_Travel_files','shapeimage_2','2');IMpreload('NYU_Travel_files','shapeimage_2','3');IMpreload('NYU_Travel_files','shapeimage_2','4');IMpreload('NYU_Travel_files','shapeimage_2','5');IMpreload('NYU_Travel_files','shapeimage_2','6');IMpreload('NYU_Travel_files','shapeimage_2','7');IMpreload('NYU_Travel_files','shapeimage_2','8');IMpreload('NYU_Travel_files','shapeimage_2','9');IMpreload('NYU_Travel_files','shapeimage_2','10');IMpreload('NYU_Travel_files','shapeimage_2','11');IMpreload('NYU_Travel_files','shapeimage_2','12');IMpreload('NYU_Travel_files','shapeimage_2','13');IMpreload('NYU_Travel_files','shapeimage_2','14');IMpreload('NYU_Travel_files','shapeimage_2','15');IMpreload('NYU_Travel_files','shapeimage_2','16');IMpreload('NYU_Travel_files','shapeimage_2','17');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
